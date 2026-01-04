eb6f8973fa341756f03e5251eb3de21e865181f1
Aniket <ananiket.pshinde18@gmail.com>
2026-01-04 10:10:42 +0530

    refactor: Remove unused compression plugin import from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { compression } from 'vite-plugin-compression2'
 
 // https://vite.dev/config/
 export default defineConfig({
